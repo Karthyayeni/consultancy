96d2e173d81cecf57a09ca51576e7ab562de341f
Karthyayeni_ <kavyakrithi0908@gmail.com>
2025-05-21 01:06:44 +0530

    Multi branch pipeline

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,15 +1,10 @@
-// jest.config.js
 module.exports = {
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
-  moduleFileExtensions: ['js', 'jsx'],
-  moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '\\.(jpg|jpeg|png|gif|svg)$': 'identity-obj-proxy',
-  },
-  transformIgnorePatterns: ['/node_modules/(?!axios)'],
-  modulePathIgnorePatterns: ['<rootDir>/src/pages/SalesReport.js']
-
+  reporters: [
+    "default",
+    [ "jest-junit", {
+      outputDirectory: "test-results/frontends",
+      outputName: "jest-results.xml"
+    }]
+  ],
+  testEnvironment: "jsdom"
 };
